52172b71fc5c4610468000fdb56d245c66373f2c
AustinFung <cst2013050143@126.com>
2019-01-28 18:01:56 +0000

    Add

--- src/District.java
+++ src/District.java
@@ -55,8 +55,60 @@ public class District
 	{
 		double maxValue = 0.0;
 		Incident highestValueIncident = null;
-		
-		
+		for(Incident currentIncident : incidentList) 
+		{
+			if(currentIncident.getValue() > maxValue) 
+			{
+				highestValueIncident = currentIncident;
+				maxValue = currentIncident.getValue();
+			}
+		}
 		return highestValueIncident;
+	}
+	
+	/**
+	 * Calculate the average incident value
+	 * @return average value
+	 */
+	public double findAverageValue(int year)
+	{
+		int totalVal = 0;
+		int numIncidents = 0;
+		double average = 0.0;
+		
+		for(Incident currentIncident : incidentList)
+		{
+			if(currentIncident.getYear() == year)
+			{
+				totalVal += currentIncident.getValue();
+				numIncidents++;
+			}
+		}
+		if(numIncidents > 0)
+		{
+			average = totalVal/numIncidents;
+		}
+		else 
+		{
+			average = 0;
+		}
+		return average;
+	}
+	
+	/**
+	 * Make a list of all incidents greater than a given value
+	 * @return list of incidents with value higher than given value
+	 */
+	public ArrayList<Incident> crimesHigherThan(double maxValue)
+	{
+		ArrayList<Incident> bigList = new ArrayList<>();
+		for(Incident currentIncident : incidentList)
+		{
+			if(currentIncident.getValue() > maxValue)
+			{
+				bigList.add(currentIncident);
+			}
+		}
+		return bigList;
 	}
 }
